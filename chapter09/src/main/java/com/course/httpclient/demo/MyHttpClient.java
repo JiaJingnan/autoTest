24a7d248dd74ab6b2e0aa17d33819b478f062825
Jia Jingnan <875480307@qq.com>
2019-08-28 11:40:27 +0800

    moco demo提交

---
+++ chapter09/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,24 @@
+package com.course.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+
+    @Test
+    public void test1() throws IOException {
+        String result;
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+
+    }
+}
